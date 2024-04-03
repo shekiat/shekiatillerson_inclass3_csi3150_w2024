57c67290ad476b139e3f722dd908b3e2718a63ed
Shekia Tillerson <shekia313tillerson@gmail.com>
2024-04-03 18:25:58 -0400

    created the counter component with a display box, and increase and decrease buttons.

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Counter from './Components/Counter';
 
 function App() {
   return (
+    // Returns the Counter component that will be displayed in the browser
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
+      
     </div>
   );
 }
--- counter-app/src/Components/Counter.js
+++ counter-app/src/Components/Counter.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Counter.css';
 
+// function that returns a counter component
 function Counter() {
-  return (
-    <div>Counter</div>
-  )
+    // sets the initial value of the count variable to 0
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            {/* displays the updated value in the counter */}
+            <div className="counter-display">{count}</div>
+            
+            {/* buttons that increase and decrease the counter number when clicked */}
+            <div className="button-container">
+                <button onClick={() => setCount(count + 1)}>+</button>
+                <button onClick={() => setCount(count - 1)}>-</button>
+            </div>
+        </div>
+    );
 }
 
-export default Counter
+export default Counter;
